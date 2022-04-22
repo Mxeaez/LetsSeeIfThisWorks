9849b3f07a8fd91c708aded3ca7ee4d6ab3b0779
Brody Hughes <bodyhughes@gmail.com>
2022-04-22 11:16:24 -0700

    Added GLFW submodule, Set up Premake, and removed ignored files from repo

---
+++ LetsSeeIfThisWorks/src/Source.cpp
@@ -0,0 +1,54 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize glfw" << std::endl;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "LetsSeeIfThisWorks"
+	architecture "x64"
+	configurations { "Debug", "Release" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "Submodules/GLFW/include"
+IncludeDir["GLAD"] = "Submodules/GLAD/include"
+
+include "Submodules/GLFW"
+
+project "LetsSeeIfThisWorks"
+	location "LetsSeeIfThisWorks"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"Submodules/GLAD/src/glad.c"
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.GLAD}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
+	}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
